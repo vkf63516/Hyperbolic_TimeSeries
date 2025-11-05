f1fff03dd64a967a650bbd3eb6d85eec76e190a3
Vishal Fenn <vkf635@gmail.com>
2025-11-05 13:55:03 -0500

    Created MLP embed files for lorentz

--- weather_euclidean.sh
+++ weather_euclidean.sh
@@ -1,12 +1,12 @@
 python run.py \
   --is_training 1 \
-  --model_id Weather_384_96_exp1_HScales \
+  --model_id Weather_336_96_exp1_HScales \
   --model HyperbolicForecasting \
   --data custom_decomposition \
   --root_path ./time-series-dataset/dataset/weather/ \
   --data_path weather.csv \
   --features MS \
-  --seq_len 384 \
+  --seq_len 336 \
   --pred_len 96 \
   --embed_dim 32 \
   --hidden_dim 256 \
@@ -16,7 +16,7 @@ python run.py \
   --use_decomposition \
   --enc_in 21 \
   --use_tensorboard \
-  --patience 5
+  --patience 6
   --manifold_type "Euclidean" \
   --use_hierarchy \
   --use_amp
